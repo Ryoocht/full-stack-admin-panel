d6c24c688eead8215d1a23a383c259329832782f
Ryoocht <ryo1.t.zubu@gmail.com>
2023-02-23 21:26:00 +1000

    added: RTK Query

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,12 +6,18 @@ import globalReducer from './state/index'
 import { Provider, TypedUseSelectorHook } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { api } from './state/api'
 
 const store = configureStore({
   reducer: {
-    global: globalReducer
-  }
+    global: globalReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: getDefault => getDefault().concat(api.middleware),
 })
+setupListeners(store.dispatch)
+
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export const useAppDispatch = () => useDispatch<AppDispatch>()
